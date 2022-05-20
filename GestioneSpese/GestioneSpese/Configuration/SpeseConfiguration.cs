f6ff6659b0a87f7af49ce41aa97e6e6a4a7fb4b1
Anatoliscame <anatoliascamerroni@gmail.com>
2022-05-20 19:15:31 +0200

    Update SpeseConfiguration.cs

--- GestioneSpese/GestioneSpese/Configuration/SpeseConfiguration.cs
+++ GestioneSpese/GestioneSpese/Configuration/SpeseConfiguration.cs
@@ -8,14 +8,16 @@ namespace GestioneSpese.Configuration
     {
         public void Configure(EntityTypeBuilder<Spese> builder)
         {
-            builder.ToTable("Spese");
+           builder.ToTable("Spese");
             builder.HasKey(d => d.Id);
             builder.Property(d => d.Data).IsRequired();
             builder.Property(d => d.Descrizione).IsRequired().HasMaxLength(500);
-            builder.Property(d => d.Utente).IsRequired().HasMaxLength(100); ;
-            //
+            builder.Property(d => d.Utente).IsRequired().HasMaxLength(100);
+            builder.Property(d => d.Importo).IsRequired();  
+            builder.Property(d => d.Approvato).IsRequired();
+    
+      
             builder.HasOne(r => r.Categoria).WithMany(d => d.Spese).HasForeignKey(r => r.CategoriaId).HasConstraintName("SpeseCategorieOneMany");
-
         }
     }
 }
