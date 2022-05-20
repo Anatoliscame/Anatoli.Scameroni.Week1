1093a5497463d4bae3df7172d666a22156848c6e
Anatoliscame <anatoliascamerroni@gmail.com>
2022-05-20 19:52:35 +0200

    Add files via upload

---
+++ GestioneSpese/GestioneSpese/BusinessLayerEF/IBusinessLayer.cs
@@ -0,0 +1,27 @@
+﻿using GestioneSpese.Entita;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestioneSpese.BusinessLayerEF
+{
+    internal interface IBusinessLayer
+    {
+
+        //Elenco dei metodi/funzionalità esposte
+        ICollection<Categorie> GetAllCategorie();
+        Categorie AggiungiCategorie(Categorie nuovoCategorie);
+        Categorie ModificaCategorie(int numR, string? nuovaCat);
+        Categorie EliminaCategorie(int idCategorieDaEliminare);
+
+        ICollection<Spese> GetAllSpese();
+        Spese ModificaSpese(string? Id, DateTime Data, string? Descrizione, string? Utente, decimal Importo, bool Approvato, int Categorie);
+        Spese InserisciNuovoSpese(Spese nuovoSpese);
+        Spese ModificaDescrizioneSpese(int Id, string? descr);
+        Spese EliminaSpese(int Id);
+        ICollection<Spese> GetSpeseById(int Id);
+
+    }
+}
