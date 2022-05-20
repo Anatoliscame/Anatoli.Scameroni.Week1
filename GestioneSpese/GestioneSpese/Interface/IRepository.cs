37025a8c346dff0d0084db17a4af087c3b93f80c
Anatoliscame <anatoliascamerroni@gmail.com>
2022-05-20 15:56:13 +0200

    Add files via upload

---
+++ GestioneSpese/GestioneSpese/Interface/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestioneSpese.Interface
+{
+    internal interface IRepository<T>
+    {
+        //operazioni CRUD
+        public ICollection<T> GetAll();
+        //public List<T> GetAll();
+        public T Add(T item);
+        public T Update(T item);
+        public bool Delete(T item);
+    }
+}
---
+++ GestioneSpese/GestioneSpese/RepositoryEF/RepositorySpeseEF.cs
@@ -0,0 +1,62 @@
+﻿using GestioneSpese.Entita;
+using GestioneSpese.Interface;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestioneSpese.RepositoryEF
+{
+    internal class RepositorySpeseEF : IRepositorySpese
+    {
+        public Spese Add(Spese item)
+        {
+            using (var ctx = new SpeseCategorieContext())
+            {
+                ctx.Spese.Add(item);
+                ctx.SaveChanges();
+            }
+            return item;
+        }
+
+        public bool Delete(Spese item)
+        {
+            using (var ctx = new SpeseCategorieContext())
+            {
+                ctx.Spese.Remove(item);
+                ctx.SaveChanges();
+            }
+            return true;
+        }
+
+        public ICollection<Spese> GetAll()
+        {
+            using (var ctx = new SpeseCategorieContext())
+            {
+                return ctx.Spese.Include(p => p.Categoria).ToList();
+
+            }
+        }
+
+        public int GetById(int id)
+        {
+            using (var ctx = new SpeseCategorieContext())
+            {
+                 return ctx.Spese.Include(x => x.Categoria).FirstOrDefault(p => p.CategoriaId == id);
+         
+            }
+        }
+
+        public Spese Update(Spese item)
+        {
+            using (var ctx = new SpeseCategorieContext())
+            {
+                ctx.Spese.Update(item);
+                ctx.SaveChanges();
+            }
+            return item;
+        }
+    }
+}
