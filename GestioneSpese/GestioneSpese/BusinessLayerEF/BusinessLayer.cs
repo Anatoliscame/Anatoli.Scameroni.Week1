b4f0671f414722ae0b3df618d88ca043b2c76e68
Anatoliscame <anatoliascamerroni@gmail.com>
2022-05-21 20:37:08 +0200

    Update BusinessLayer.cs

--- GestioneSpese/GestioneSpese/BusinessLayerEF/BusinessLayer.cs
+++ GestioneSpese/GestioneSpese/BusinessLayerEF/BusinessLayer.cs
@@ -4,9 +4,82 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace GestioneSpese.BusinessLayerEF
 {
-    internal class BusinessLayer
+    public class BusinessLayer : IBusinessLayer
     {
+            private readonly IRepositorySpese speseRepo;
+            private readonly IRepositoryCategorie categorieRepo;
+
+            public BusinessLayer(IRepositorySpese spese, IRepositoryCategorie categorie)
+            {
+                speseRepo = spese;
+                categorieRepo = categorie;
+            }
+
+
+        public ICollection<Categorie> GetAllCategorie()
+        {
+           return categorieRepo.GetAll();
+        }
+
+        public ICollection<Spese> GetAllSpese()
+        {
+            return speseRepo.GetAll();
+        }
+
+        public Spese InserisciNuovoSpese(Spese nuovoSpese)
+        {
+          Categorie categorieRecuperato = categorieRepo.GetByIdC(nuovoSpese.CategoriaId);
+            if (categorieRecuperato ==null)
+            {
+            return null;
+            }
+           return speseRepo.Add(nuovoSpese);
+           
+          
+        }
+
+
+        public Categorie AggiungiCategorie(Categorie nuovoCategorie)
+        {
+            //Se Id di una Categoria non esiste viene generato nuovo oggetto
+            Categorie categorieRecuperato = categorieRepo.GetByIdC(nuovoCategorie.Id);
+           if (categorieRecuperato == null)
+            {
+                return categorieRepo.Add(nuovoCategorie);
+
+            }
+             return null;
+  }
+
+
+        public Categorie EliminaCategorie(int idCategorieDaEliminare)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Spese EliminaSpese(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+       
+
+        public Categorie ModificaCategorie(int numR, string? nuovaCat)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Spese ModificaDescrizioneSpese(int Id, string? descr)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Spese ModificaSpese(string? Id, DateTime Data, string? Descrizione, string? Utente, decimal Importo, bool Approvato, int Categorie)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
